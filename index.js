df167b7178901b9b3fe16c54ca5d2e3e60ef96dd
Leonel Goncalves <devok.software@gmail.com>
2024-10-23 11:24:28 +0200

    update season to get data

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const allowedOrigins = [
 ];
 
 const requestOptions = {
-  uri: "https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2023",
+  uri: "https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2024",
   method: "GET",
   headers: {
     "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
